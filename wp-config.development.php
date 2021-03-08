25895b64ec701972dcc2f0f052001fa8cb4ab705
mlapalus <mlapalus@yahoo.fr>
2021-03-08 22:32:56 +0100

    add config for dev and prod

---
+++ wp-config.development.php
@@ -0,0 +1,13 @@
+<?php
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define('DB_NAME', 'bureau-database');
+
+/** Utilisateur de la base de données MySQL. */
+define('DB_USER', 'root');
+
+/** Mot de passe de la base de données MySQL. */
+define('DB_PASSWORD', 'root');
+
+/** Adresse de l’hébergement MySQL. */
+define('DB_HOST', 'localhost');
---
+++ wp-config.env.php
@@ -0,0 +1,12 @@
+<?php
+switch ($hostname) {
+  case 'localhost':
+    define('WP_ENV', 'development');
+    break;
+  case 'http://inspiration-coiffure.herokuapp.com':
+    define('WP_ENV', 'production');
+    break;
+  default:
+    die("Erreur de configuration de l'environement");
+    define('WP_ENV', 'production');
+}
